52f53e36bf98d358cb1ffe9566302ed43b3d6001
Larisa Marcinevskaya <marcinevskaya93@mail.ru>
2021-06-19 23:11:24 +0300

    Final version

--- HostingAppGuessNumber/ContentView.swift
+++ HostingAppGuessNumber/ContentView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var targetValue = Int.random(in: 1...100)
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+           GameSlider(
+            currentValue: $currentValue,
+            targetValue: targetValue,
+            color: .red,
+            alpha: computeScore()
+           )
+            
+            ButtonView(
+                showAlert: $showAlert,
+                title: "Проверь меня!",
+                currentScore: computeScore(),
+                action: { showAlert = true }
+            )
+            
+            ButtonView(title: "Начать заново") {
+                targetValue = Int.random(in: 1...100)
+            }
+        }
+    }
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
+
+struct ButtonView: View {
+    @Binding var showAlert: Bool
+    let title: String
+    let currentScore: Int
+    let action: () -> Void
+    
+    var body: some View {
+        Button(title, action: action)
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Your Score"),
+                message: Text("\(currentScore)")
+            )
+        }
+        .padding()
+    }
+    
+    init(showAlert: Binding<Bool> = .constant(false), title: String, currentScore: Int = 0, action: @escaping () -> Void) {
+        self._showAlert = showAlert
+        self.title = title
+        self.currentScore = currentScore
+        self.action = action
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
